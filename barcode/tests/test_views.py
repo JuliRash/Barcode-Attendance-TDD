bfb8652519b670c7cb9a7de9b4958e90de34f0cb
julius idowu i.o <idowujulius92@gmail.com>
2021-03-17 20:08:21 +0100

    update basic view to return minimal HTML

--- barcode/tests/test_views.py
+++ barcode/tests/test_views.py
@@ -9,3 +9,11 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEquals(found.func, home_page)
 
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>Barcode Attendance</title>', html)
+        self.assertTrue(html.endswith('</html>'))
+
