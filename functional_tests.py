e5173ff1f0627fc6baa60272550fd242ee616583
julius idowu i.o <idowujulius92@gmail.com>
2021-03-18 16:03:33 +0100

    Functional Test now checks if we can input id-number of user

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 import unittest
+import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from decouple import config
 
 
@@ -11,16 +13,30 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_submit_attendance(self):
         # Edith has heard about a cool new online barcode attendance system.
         # she goes to check out its homepage.
         self.browser.get('http://localhost:8000')
 
-        # she notices the page title and header mentions attendance.
+        # she notices the page title and header mentions Attendance.
         self.assertIn('Attendance', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Attendance', header_text)
+        # she is invited to input her identity number straight away.
+        input_box = self.browser.find_element_by_id('id_number')
+        self.assertEqual(
+            input_box.get_attribute('placeholder'),
+            'Enter ID Number')
+        # she types "0909" into the text box and clicks enter
+        input_box.send_keys('0909')
+        # when she hits enter, the page sends a post request to the mark attendance end-point,
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+        # if the number is correct it redirects her to marked attendance page,
+        # and shows her her information containing [full_name, id_number, phone_number]
+        # "Her Full name
         self.fail('Finish the test ✔️')
 
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
-
