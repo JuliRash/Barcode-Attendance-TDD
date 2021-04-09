7673f78576ef64e5ac21eff4bbd137b87e156a12
julius idowu i.o <idowujulius92@gmail.com>
2021-04-09 15:47:24 +0100

    refactor home_page view

--- barcode/views.py
+++ barcode/views.py
@@ -1,18 +1,21 @@
-from django.shortcuts import redirect, render
-from django.http import HttpResponse
+from django.shortcuts import render
+from datetime import date
 
-from barcode.models import Attendance, Person
+from barcode.models import Attendance, Person, Setup
 from barcode.decorators import setup_is_configured
 
 
 @setup_is_configured
 def home_page(request):
+    setup = Setup.objects.first()
+    today_date = date.today()
     if request.method == 'POST':
         try:
             person = Person.objects.get(code=request.POST['code'])
-            attendance = Attendance.objects.create(person=person)
-            context = {'attendance': attendance}
+            attendance = Attendance.objects.save_attendance(person=person, date=today_date.day)
+            context = {'attendance': attendance, 'setup': setup}
         except Person.DoesNotExist:
-            context = {'error': 'Error: This user does not exist.'}
+            context = {'error': 'Error: This user does not exist.', 'setup': setup}
         return render(request, 'home.html', context)
-    return render(request, 'home.html')
+    context = {'setup': setup}
+    return render(request, 'home.html', context)
