046442c4875704275476316132aad5d1f2b1a5e8
julius idowu i.o <idowujulius92@gmail.com>
2021-04-09 15:44:46 +0100

    add Person Model to admin

--- barcode/admin.py
+++ barcode/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from barcode.models import Setup
+from barcode.models import Setup, Person
 
 
 @admin.register(Setup)
 class SetupAdmin(admin.ModelAdmin):
     list_display = ['organization_name', 'organization_location']
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'code']
