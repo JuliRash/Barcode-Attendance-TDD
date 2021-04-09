b6f012a4914eef813889987c5e427f0530c48275
julius idowu i.o <idowujulius92@gmail.com>
2021-04-09 15:39:45 +0100

    modify Attendance model field time_out

--- barcode/models.py
+++ barcode/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from datetime import datetime
 
+from barcode.managers import AttendanceManager
+
 
 class Person(models.Model):
     first_name = models.CharField(max_length=255)
@@ -9,17 +11,25 @@ class Person(models.Model):
     email = models.EmailField()
     code = models.CharField(max_length=255)
 
+    class Meta:
+        verbose_name_plural = 'People'
+
+    def get_full_name(self):
+        return self.first_name + ' ' + self.last_name
+
     def __str__(self):
-        return self.id_number
+        return self.code
 
 
 class Attendance(models.Model):
     person = models.ForeignKey(Person, on_delete=models.CASCADE)
     date = models.DateField(default=datetime.now)
     time_in = models.TimeField(default=datetime.now)
-    time_out = models.TimeField(blank=True, default=datetime.now)
+    time_out = models.TimeField(blank=True, null=True)
     purpose = models.CharField(max_length=255, blank=True)
     location = models.CharField(max_length=255, default='Barcode Attendance')
+
+    objects = AttendanceManager()
 
     def __Str__(self):
         return self.person
@@ -41,6 +51,3 @@ class Setup(models.Model):
     def save(self, *args, **kwargs):
         self.id = 1
         return super().save(*args, **kwargs)
-
-
-
