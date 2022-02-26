5f5ac97d03ce9e2f1253ef8869506b7bdf347c43
Idowu Julius <idowujulius92@gmail.com>
2022-02-26 15:07:55 +0100

    update urls

--- barcode/urls.py
+++ barcode/urls.py
@@ -1,7 +1,13 @@
+from unicodedata import name
 from django.urls import path
 
 from barcode import views
 
 urlpatterns = [
     path('', views.home_page, name='home_page'),
+    path('barcode', views.barcode_attendance, name='barcode_attendance'),
+    path('mark-barcode-attendance',
+         views.mark_barcode_attendance, name='mark-attendance'),
+    path('finger-print', views.finger_print_scanner,
+         name='finger-print_attendance')
 ]
